d35b36bf2faf6c9a455ca1ea8532b53e89aa0edc
Opilane TTHK <opilane@tthk.ee>
2021-09-15 12:23:53 +0300

    Week3Exercises

---
+++ DateAndTimeObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateAndTimeObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime current = DateTime.Now;
+            Console.WriteLine(current.Hour);
+            Console.WriteLine(current.Minute);
+            Console.WriteLine(current.Second);
+            Console.WriteLine(current.ToString("MMMM"));
+            Console.WriteLine(current.ToString("yyyy"));
+            Console.WriteLine(current.ToString("dddd dd, MMMM"));
+            
+        }
+    }
+}
